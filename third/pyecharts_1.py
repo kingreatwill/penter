b1576d29c3011b23573f474716509134ff6ff640
kingreatwill <350840291@qq.com>
2020-01-04 17:23:48 +0800

    python 学习

---
+++ third/pyecharts_1.py
@@ -0,0 +1,17 @@
+# https://pyecharts.org/#/zh-cn/quickstart
+# https://github.com/pyecharts/pyecharts
+from pyecharts.charts import Bar
+
+bar = Bar()
+bar.add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+bar.add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+# render 会生成本地 HTML 文件，默认会在当前目录生成 render.html 文件
+# 也可以传入路径参数，如 bar.render("mycharts.html")
+bar.render()
+
+# bar = (
+#     Bar()
+#     .add_xaxis(["衬衫", "羊毛衫", "雪纺衫", "裤子", "高跟鞋", "袜子"])
+#     .add_yaxis("商家A", [5, 20, 36, 10, 75, 90])
+# )
+# bar.render()
