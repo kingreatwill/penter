034b49a3066fd4ef03a7079055bee5722cb860f9
kingreatwill <350840291@qq.com>
2020-06-10 16:29:34 +0800

    pandas

---
+++ pandas/exercises/README.md
@@ -0,0 +1,1 @@
+https://github.com/guipsamora/pandas_exercises
--- spark-py/demo/kafka_producer.py
+++
@@ -1,6 +0,0 @@
-from kafka import KafkaProducer
-producer = KafkaProducer(bootstrap_servers=['192.168.110.150:9092','192.168.110.151:9092','192.168.110.152:9092'])
-
-producer.send('kafka_sql_stream_topic', b'x.x,x x')
-
-producer.close()
