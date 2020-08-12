01fd04ad721b227c006ad5cf57faec2f8d954b7c
kingreatwill <kingreatwill@qq.com>
2020-08-12 14:48:53 +0800

    推荐系统.

--- third/imagelib/03_pytesseract.py
+++ third/imagelib/03_pytesseract.py
@@ -7,13 +7,13 @@
 # E:\bigdata\ai\tesseract-4.0.0-alpha
 # 下载训练数据 https://tesseract-ocr.github.io/tessdoc/Data-Files.html
 # 拷贝文件到E:\bigdata\ai\tesseract-4.0.0-alpha\tessdata文件夹
-# pip install pytesseract
+# pip install pytesseract -i  http://mirrors.aliyun.com/pypi/simple/ --trusted-host mirrors.aliyun.com
 from PIL import Image
 
 import pytesseract
 
 # If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'E:\bigdata\ai\tesseract-4.0.0-alpha\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r'F:\bigdata\ai\Tesseract-OCR\tesseract.exe'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 # Simple image to string
