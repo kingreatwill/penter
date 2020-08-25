e5e2bcc0575c49289d0f412f557ab15874c3ad98
kingreatwill <kingreatwill@qq.com>
2020-08-25 11:47:04 +0800

    github ip

--- example/githubhosts.py
+++ example/githubhosts.py
@@ -47,7 +47,8 @@ def ip138_output_hosts(domains):
     import requests
     import time
     import json
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
     for domain in domains:
         resp = requests.get(url='https://site.ip138.com/domain/read.do', headers=headers,
                             params={'domain': domain, 'time': int(round(time.time() * 1000))})
@@ -60,6 +61,56 @@ def ip138_output_hosts(domains):
     # https://site.ip138.com/domain/read.do?domain=raw.githubusercontent.com&time=1598249072506 #毫秒级时间戳，13位
 
 
+# ipaddress.com (java 版本：https://gitee.com/bryan31/githubhost/)
+def ipaddress_output_hosts(domains):
+    import os
+    import sys
+    import requests
+    import json
+    import tldextract # install tldextract
+    from bs4 import BeautifulSoup  # install beautifulsoup4
+    from pythonping import ping # install pythonping
+    # https://github.com/alessandromaggio/pythonping 也可以试下 https://github.com/romana/multi-ping
+
+    print("# windows: ipconfig /flushdns \n# linux:sudo killall -HUP mDNSResponder")
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.105 Safari/537.36'}
+    # https://githubusercontent.com.ipaddress.com/avatars3.githubusercontent.com
+    for domain in domains:
+        result = tldextract.extract(domain)
+        if result.registered_domain == domain:
+            url = "https://{}.ipaddress.com".format(domain)
+        else:
+            url = "https://{}.ipaddress.com/{}".format(result.registered_domain, domain)
+
+        resp = requests.get(url=url, headers=headers)
+        if resp.ok:
+            try:
+                soup = BeautifulSoup(resp.text, 'html.parser')
+                has_ip_table = soup.select('table[class="panel-item table table-stripes table-v"]')[0]
+                ips = has_ip_table.select('tr')[-1].select('td ul li')
+                curr_ip = None
+                curr_ms = sys.maxsize
+                if len(ips) == 1:
+                    curr_ip = ips[0].text
+                else:
+                    for ip in ips:
+                        if not curr_ip:
+                            curr_ip = ip.text
+                        # result = os.popen('ping -c 1 {}'.format(ip.text)).read()
+                        # result = os.popen('ping -n 10 {}'.format(ip.text)).read() # win
+                        p_r = ping(ip.text, timeout=5, count=10, verbose=False)
+                        if p_r.rtt_avg_ms < curr_ms:
+                            curr_ip = ip.text
+                            curr_ms = p_r.rtt_avg_ms
+                if not curr_ip:
+                    print("# {} 没有找到IP。".format(curr_ip, domain))
+                else:
+                    print("{} {}".format(curr_ip, domain))
+            except Exception:
+                print("# domain:{}, 没有找到IP.".format(domain))
+
+
 if __name__ == '__main__':
     # haiwai_output_hosts(need_domains)
-    ip138_output_hosts(need_domains)
+    ipaddress_output_hosts(need_domains)
