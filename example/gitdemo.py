3c146517a75d62d0805525dd30f6e471ee0ee0d2
kingreatwill <kingreatwill@qq.com>
2020-08-27 16:24:31 +0800

    git

--- example/gitdemo.py
+++ example/gitdemo.py
@@ -1,12 +1,18 @@
 # https://github.com/dotnet/roslyn.git
 import argparse
+import io
 import re
 import os
-
 
 # 为了减少依赖还是直接命令号了， pip install GitPython
 
 # 项目名称不能重复
+import subprocess
+import sys
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
+
+
 def read_git_list(filename):
     dic = {}
     with open(filename, 'r') as f:
@@ -32,16 +38,27 @@ def main(path, name, git_prefix):
         repo_path = os.path.join(path, proj_name)
         # 不存在：clone( 还可以检查是否是git仓库，有时存在目录却不是git仓库，那么可以删除)
         if not os.path.exists(repo_path):
-            output = os.popen("git clone {} {}".format(repo_url, repo_path))
-            print(output.read())
+            xx = popen("git clone {} {}".format(repo_url, repo_path)).read()
+
         # git remote -v
-        output = os.popen("cd {} & git remote -v".format(repo_path))
-        # git remote set-url --add origin git@e.coding.net:openjw/kingreatwill/open.git
+        output = popen("cd /d {} & git remote -v".format(repo_path))
+        # output = os.popen("cd /d {} & ls".format(repo_path))
         sync_url = "{}/{}.git".format(git_prefix, proj_name)
-        if output.read().find(sync_url) < 0:
-            print(os.popen("git remote set-url --add origin {}".format(sync_url)).read())
+        git_remote_v = output.read()
+        if git_remote_v.find(sync_url) < 0:
+            print(popen("cd /d {} & git remote set-url --add origin {}".format(repo_path, sync_url)).read())
         # pull repo, push repo
-        print(os.popen("cd {} & git pull & git push".format(repo_path)).read())
+        # print(popen('cd {} & git pull & git push'.format(repo_path)).read())
+        # out_cmd = popen(
+        #     ["git-bash.exe", "--no-needs-console", "--hide", "-c",
+        #      'cd "{}" && git pull && GIT_SSH_COMMAND="ssh -i {}" git push'.format(repo_path, os.path.join(path, "demo"))])
+        # print(out_cmd.read())
+
+
+def popen(cmd):
+    proc = subprocess.Popen(cmd, shell=True, stdin=None,
+                            stdout=subprocess.PIPE)
+    return os._wrap_close(io.TextIOWrapper(proc.stdout, encoding='utf-8'), proc)
 
 
 parser = argparse.ArgumentParser()
