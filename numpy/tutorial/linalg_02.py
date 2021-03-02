7e585fa1dec3a9b9bcb731acd2cd1b547c5bf5ac
kingreatwill <350840291@qq.com>
2021-03-02 17:30:50 +0800

    线性代数。

--- numpy/tutorial/linalg_02.py
+++ numpy/tutorial/linalg_02.py
@@ -4,6 +4,20 @@ from sympy import Matrix
 A = np.array([[2, 3, 1, 0], [4, -1, 9, 0], [3, 8, -2, 0], [1, -2, 4, 0]])
 # 将numpy.array转为sympy.Matrix
 MA = Matrix(A)
+print("------------------行简化阶梯型-------------------")
 print(MA.rref()) # 行简化阶梯型
 
+
+print("------------------对角化矩阵-------------------")
+M = Matrix([[3,-2,4,-2],[5,3,-3,-2],[5,-2,2,-2],[5,-2,-3,3]])
+P, D = M.diagonalize() # 对角化矩阵
+print(P)
+print("对角矩阵 D：")
+print(D)
+
+print("------------------LU 分解-------------------")
 print(MA.LUdecomposition()) # LU 分解
+print("------------------QR 分解-------------------")
+print(MA.QRdecomposition()) # QR 分解
+# (把矩阵分解成一个正交矩阵与一个上三角矩阵的积 ,Q 是正交矩阵（意味着 QTQ = I）而 R 是上三角矩阵,此外，原矩阵A不必为正方矩阵； 如果矩阵A大小为n*m，则矩阵Q大小为n*m，矩阵R大小为m*m。)
+
