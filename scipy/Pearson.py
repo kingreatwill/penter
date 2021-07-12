a666c3123273a4c17f0bee6c8829702a5ecc9ebc
kingreatwill <350840291@qq.com>
2021-07-12 17:30:49 +0800

    机器学习

---
+++ scipy/Pearson.py
@@ -0,0 +1,9 @@
+import numpy as np
+from scipy.stats import pearsonr
+from scipy.spatial.distance import euclidean
+from scipy.spatial.distance import cosine
+from sklearn.preprocessing import StandardScaler
+"""
+Pearson相关性系数（Pearson Correlation）是衡量向量相似度的一种方法。输出范围为-1到+1, 0代表无相关性，负值为负相关，正值为正相关。
+[如何理解皮尔逊相关系数（Pearson Correlation Coefficient）](https://www.zhihu.com/question/19734616/answer/349132554)
+"""
